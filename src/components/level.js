dfa58be1f0bd348e2b494d1f3bc85fa75c3a1317
Uzair Ashraf <uzinatorcl@live.com>
2020-09-09 00:15:36 -0700

    character sprites modified, man that was a pain

--- src/components/level.js
+++ src/components/level.js
@@ -1,5 +1,10 @@
+import map1 from '../../assets/images/maps/map1.png'
+
 export default class Level {
   constructor() {
+    this.tileMap = this.generateTileMap()
+  }
+  generateTileMap() {
 
   }
 }
